2a491474348ba6e872e56b4cdecf37f1d795017c
velkanknight <velkanknight@gmail.com>
2025-11-03 23:23:05 -0300

    change port

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.post('/search-flights', async (req, res) => {
 });
 
 // Iniciar o servidor
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
